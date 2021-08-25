edd32f1679470a286c652e72f3aa8e529c6e4e2c
Your Name <you@example.com>
2021-08-25 16:26:02 +0200

    changes

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ class User(object):
         self.username = username
         self.password = password
 
+class Dentist(object):
+    def __init__(self, dentist_id, dentist_username, dentist_password):
+        self.id = dentist_id
+        self.username = dentist_username
+        self.password = dentist_password
 
 class Admin(object):
     def __init__(self, admin_id, admin_username, admin_password):
@@ -24,7 +29,7 @@ class Admin(object):
 
 class Database(object):
     def __init__(self):
-        self.conn = sqlite3.connect('hotel.db')
+        self.conn = sqlite3.connect('dentist.db')
         self.cursor = self.conn.cursor()
 
     def sending_to_database(self, query, values):
@@ -44,7 +49,7 @@ class Database(object):
 
 # fetching users from the database
 def fetch_users():
-    with sqlite3.connect('hotel.db') as conn:
+    with sqlite3.connect('dentist.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM user")
         users_data = cursor.fetchall()
@@ -59,7 +64,7 @@ def fetch_users():
 
 # creating table for users
 def init_user_table():
-    conn = sqlite3.connect('hotel.db')
+    conn = sqlite3.connect('dentist.db')
     print("Opened database successfully")
 
     conn.execute("CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY,"
@@ -79,7 +84,7 @@ users = fetch_users()
 
 # creating table for the products
 def init_products_table():
-    with sqlite3.connect('hotel.db') as conn:
+    with sqlite3.connect('dentist.db') as conn:
         conn.execute("CREATE TABLE IF NOT EXISTS room (room_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                      "room_number TEXT NOT NULL,"
                      "description TEXT NOT NULl,"
@@ -95,7 +100,7 @@ userid_table = {u.id: u for u in users}
 
 
 def fetch_admin():
-    with sqlite3.connect('hotel.db') as conn:
+    with sqlite3.connect('dentist.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM admin")
         users_data = cursor.fetchall()
@@ -125,7 +130,7 @@ adminid_table = {a.admin_id: a for a in admin}
 
 
 def init_appointment_table():
-    with sqlite3.connect('hotel.db') as conn:
+    with sqlite3.connect('dentist.db') as conn:
         conn.execute("CREATE TABLE IF NOT EXISTS appointment(appointment_id INTEGER PRIMARY KEY AUTOINCREMENT,"
                      "date_made TEXT NOT NULL,"
                      "appointment_date,"
@@ -158,7 +163,7 @@ app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 app.config['CORS-HEADERS'] = ['Content-Type']
 mail = Mail(app)
-jwt = JWT(app, authenticate, identity)
+# jwt = JWT(app, authenticate, identity)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 
@@ -182,8 +187,8 @@ def user_registration():
             values = (user_id, first_name, surname, address, email, username, password)
             db.sending_to_database(query, values)
 
-            message = Message('Thank You', sender='justtotestmywork@gmail.com', recipients=[email])
-            message.body = "Thank you for registering happy shopping"
+            message = Message('Thank You', sender='aaliyahjardien04@gmail.com', recipients=[email])
+            message.body = "Thanks for "
             mail.send(message)
             response["message"] = 'Success'
             response["status_code"] = 201
@@ -243,7 +248,7 @@ def login():
     if request.method == "POST":
         username = request.json["admin_username"]
         password = request.json["admin_password"]
-        conn = sqlite3.connect("hotel.db")
+        conn = sqlite3.connect("dentist.db")
         c = conn.cursor()
         statement = (f"SELECT * FROM admin WHERE admin_username='{username}' and admin_password ="
                      f"'{password}'")
@@ -263,7 +268,7 @@ def login():
 @app.route('/show-appointments/', methods=['GET'])
 def show_users():
     response = {}
-    with sqlite3.connect('hotel.db') as conn:
+    with sqlite3.connect('dentist.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM appointment")
 
@@ -277,7 +282,7 @@ def show_users():
 @app.route('/show-user/<username>', methods=["GET"])
 def view_user(username):
     response = {}
-    with sqlite3.connect('hotel.db') as conn:
+    with sqlite3.connect('dentist.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM user WHERE username='" + str(username) + "'")
         response['status_code'] = 200
@@ -341,14 +346,14 @@ def edit_product(room_id):
     response = {}
 
     if request.method == "PUT":
-        with sqlite3.connect('hotel.db') as conn:
+        with sqlite3.connect('dentist.db') as conn:
             cursor = conn.cursor()
             incoming_data = dict(request.json)
             put_data = {}
 
             if incoming_data.get("room_number") is not None:
                 put_data["room_number"] = incoming_data.get("room_number")
-                with sqlite3.connect('hotel.db') as conn:
+                with sqlite3.connect('dentist.db') as conn:
                     cursor = conn.cursor()
                     cursor.execute("UPDATE room SET room_number =? WHERE room_id =?", (put_data['room_number'],
                                                                                        room_id))
@@ -358,7 +363,7 @@ def edit_product(room_id):
 
             if incoming_data.get("description") is not None:
                 put_data["description"] = incoming_data.get("description")
-                with sqlite3.connect('hotel.db') as conn:
+                with sqlite3.connect('dentist.db') as conn:
                     cursor = conn.cursor()
                     cursor.execute("UPDATE room SET description =? WHERE room_id =?",
                                    (put_data['description'], room_id))
@@ -368,7 +373,7 @@ def edit_product(room_id):
 
             if incoming_data.get("suit_type") is not None:
                 put_data["suit_type"] = incoming_data.get("suit_type")
-                with sqlite3.connect('hotel.db') as conn:
+                with sqlite3.connect('dentist.db') as conn:
                     cursor = conn.cursor()
                     cursor.execute("UPDATE room SET quantity =? WHERE room_id =?",
                                    (put_data['suit_type'], room_id))
